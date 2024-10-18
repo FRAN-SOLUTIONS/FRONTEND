30465bc77ab8b11de7af06509a72e5eee6b7e68e
Maria Helena Baldan Lemes <mariahelena1119@gmail.com>
2024-10-17 23:37:11 -0300

    style: mudança em FooterComp.vue

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,23 +2,23 @@
 </script>
 
 <template>
-     <div class="py-4 .bg-body-tertiary">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-4 mx-4 border-top mb-0">
-            <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                    <img src="../assets/images/FRAN_logo.png" width="40px" style="border-radius: 50%;">
-                </a>
-                <span class="mb-3 mb-md-0 text-body-secondary">© 2024 FRAN Solutions, Inc</span>
-            </div>
-
-            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3"><a class="text-body-secondary" href="https://suap.ifsp.edu.br/accounts/login/?next=/"><img src="../assets/images/suap_logo.png" width="40px"></a></li>
-                <li class="ms-3"><a class="text-body-secondary" href="https://eadcampus.spo.ifsp.edu.br/my/"><img src="../assets/images/moodle_logo.png" height="60px"></a></li>
-                <li class="ms-3"><a class="text-body-secondary" href="https://www.ifsp.edu.br/"><img src="../assets/images/if_logo.png" width="30px"></a></li>
-            </ul>
-        </footer>
-    </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+        <div class="col-md-4 d-flex align-items-center">
+            <router-link to="home"><img src="../assets/images/FRAN_logo.png" width="40px" style="border-radius: 50%; margin-left: 15px;"></router-link>
+        </div>
+        <div>
+            <span class="mb-3">© 2024 FRAN Solutions, Inc. Todos os direitos reservados.</span>
+        </div>
+        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-body-secondary" href="https://suap.ifsp.edu.br/accounts/login/?next=/"><img src="../assets/images/suap_logo.png" style="width: 40px; margin-right: 10px;"></a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="https://spo.ifsp.edu.br/menu/191-estagios-documentos"><img src="../assets/images/if_logo_2.png" style="width: 30px; margin-right: 20px"></a></li>
+        </ul>
+    </footer>
 </template>
 
 <style scoped>
+footer{
+    background-color: #01400B;
+    color: white;
+}
 </style>
