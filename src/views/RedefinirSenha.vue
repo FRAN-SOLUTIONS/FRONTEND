69dd3356d45c68dae7107d846f04ea662189f0f4
Maria Helena Baldan Lemes <mariahelena1119@gmail.com>
2024-10-23 12:12:58 -0300

    fix: mudança no formulário de cadastro de alunos e de redefinir senha

--- src/views/RedefinirSenha.vue
+++ src/views/RedefinirSenha.vue
@@ -1,139 +1,165 @@
 <script setup>
-import { ref } from 'vue';
-import HeaderComp from '@/components/HeaderComp.vue';
-import FooterComp from '@/components/FooterComp.vue';
-import validator from 'validator';  // Importa a biblioteca validator
+import { ref } from 'vue'
+import HeaderComp from '@/components/HeaderComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
+import validator from 'validator' // Importa a biblioteca validator
 import '../assets/css/global.css'
 
 // Variáveis reativas
-const step = ref(1); // Controla qual etapa está ativa
-const enderecoEmail = ref('');
-const novaSenha = ref('');
-const confirmaSenha= ref('');
+const step = ref(1) // Controla qual etapa está ativa
+const page = ref(1)
+
+const enderecoEmail = ref('')
+const novaSenha = ref('')
+const confirmaSenha = ref('')
 
 // Função para enviar o email e passar para a próxima etapa
 function sendEmail() {
-    if (!enderecoEmail.value) {
-        alert('Por favor, digite seu email.');
-        return;
-    }
-    else if (!validator.isEmail(enderecoEmail.value)) {  // Valida o e-mail
-        alert('Email válido');
-        return;
-    }
-    // Aqui você pode adicionar a lógica para enviar o email ao servidor
-    step.value = 2; // Avança para a etapa de redefinição de senha
+  if (!enderecoEmail.value) {
+    alert('Por favor, digite seu email.')
+    return
+  } else if (!validator.isEmail(enderecoEmail.value)) {
+    // Valida o e-mail
+    alert('Email válido')
+    return
+  }
+  // Aqui você pode adicionar a lógica para enviar o email ao servidor
+  step.value = 2 // Avança para a etapa de redefinição de senha
 }
 
 // Função para validar e redefinir a senha
 function resetPassword() {
-    if (novaSenha.value !== confirmaSenha.value) {
-        alert('As senhas não coincidem!');
-        return;
-    }
-    
-    alert('Senha redefinida com sucesso!');
+  if (novaSenha.value !== confirmaSenha.value) {
+    alert('As senhas não coincidem!')
+    return
+  }
 
-    // Lógica adicional para enviar a nova senha ao servidor
-    
-    step.value = 3;
-    
+  alert('Senha redefinida com sucesso!')
+  // Lógica adicional para enviar a nova senha ao servidor
+  page.value = 2
 }
 </script>
 
 <template>
-    <HeaderComp />
+  <HeaderComp />
 
-    <main>
-        <h2 class="text-center mt-4">Preencha os campos a seguir para redefinir sua senha:</h2>
-        
-        <div class="form-container">
-            <form>
-                <div v-if="step === 1">
-                    <div class="form-group">
-                        <label for="email" class="form-label">Email:</label>
-                        <input type="email" v-model="enderecoEmail" class="form-control" required />
+  <main>
+    <div v-if="page === 1">
+      <h2 class="text-center mt-4">
+        Preencha os campos a seguir para redefinir sua senha:
+      </h2>
 
-                        <button @click="sendEmail" class="btn-custom">Enviar</button>
-                    </div> 
-                </div>
-                <div v-else-if="step === 2">
-                    <div class="form-group">
-                        <label for="nova-Senha"  class="form-label">Nova Senha:</label>
-                        <input type="password" v-model="novaSenha" class="form-control"  required />
+      <div class="form-container">
+        <form>
+          <div v-if="step === 1">
+            <div class="form-group">
+              <label for="email" class="form-label">Email:</label>
+              <input
+                type="email"
+                v-model="enderecoEmail"
+                class="form-control"
+                required
+              />
 
-                        <label for="confirma-senha"  class="form-label">Confirme a Nova Senha:</label>
-                        <input type="password" v-model="confirmaSenha" class="form-control" required />
+              <button @click="sendEmail" class="btn-custom">Enviar</button>
+            </div>
+          </div>
+          <div v-else-if="step === 2">
+            <div class="form-group">
+              <label for="nova-Senha" class="form-label">Nova Senha:</label>
+              <input
+                type="password"
+                v-model="novaSenha"
+                class="form-control"
+                required
+              />
 
-                        <button @click.prevent="resetPassword" class="btn-custom">Redefinir</button>
-                    </div>
-                </div>
-                <div v-else>
-                    <h2>Faça login para entrar</h2>
+              <label for="confirma-senha" class="form-label"
+                >Confirme a Nova Senha:</label
+              >
+              <input
+                type="password"
+                v-model="confirmaSenha"
+                class="form-control"
+                required
+              />
 
-                    <div class="form-group">
-                        <router-link to="login" style="text-decoration: none;"><button class="btn-custom">Login</button></router-link>
-                    </div>
-                </div>
-            </form>
+              <button @click.prevent="resetPassword" class="btn-custom">
+                Redefinir
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div v-else-if="page === 2">
+      <h2>Faça login para entrar</h2>
+
+      <form class="form-container">
+        <div class="form-group">
+          <router-link to="login" style="text-decoration: none"
+            ><button class="btn-custom">Login</button></router-link
+          >
         </div>
-    </main>
+      </form>
+    </div>
+  </main>
 
-    <FooterComp />
+  <FooterComp />
 </template>
 
 <style scoped>
-h2{
-    color: #01400B; /* Define a cor */
-    text-align: center;
-    margin-top: 1.5rem !important;
+h2 {
+  color: #01400b; /* Define a cor */
+  text-align: center;
+  margin-top: 1.5rem !important;
 }
 
-main{
-    max-width: 100%; /* Aumenta a largura máxima do contêiner */
-    margin: 0 auto;
-    padding: 20px;
+main {
+  max-width: 100%; /* Aumenta a largura máxima do contêiner */
+  margin: 0 auto;
+  padding: 20px;
 }
 
 .form-container {
-    background-color: #fff;
-    padding: 20px;
-    max-width: 500px; /* Aumenta a largura máxima do contêiner */
-    margin: 50px auto;
-    margin-top: 0;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+  padding: 20px;
+  max-width: 500px; /* Aumenta a largura máxima do contêiner */
+  margin: 50px auto;
+  margin-top: 0;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .form-group {
-    margin-bottom: 15px;
+  margin-bottom: 15px;
 }
 
 .form-group label {
-    display: block;
-    margin-bottom: 5px;
+  display: block;
+  margin-bottom: 5px;
 }
 
 .form-group input {
-    width: 100%;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #EAEBEE; /* Cor de fundo das caixas de formulário */
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #eaebee; /* Cor de fundo das caixas de formulário */
 }
 
 .btn-custom {
-    display: block;
-    width: 50%;
-    padding: 10px;
-    background-color: #01400B;
-    color: #fff;
-    border-radius: 8px;
-    cursor: pointer;
-    margin: 30px auto;
+  display: block;
+  width: 50%;
+  padding: 10px;
+  background-color: #01400b;
+  color: #fff;
+  border-radius: 8px;
+  cursor: pointer;
+  margin: 30px auto;
 }
 
 .btn-custom:hover {
-    background-color: #012d08; /* Cor do botão ao passar o mouse */
+  background-color: #012d08; /* Cor do botão ao passar o mouse */
 }
 </style>
