aa685902cd8a45dad3c215c6562e575cab425e03
Luiza Vieira <lvieirasilva9@gmail.com>
2024-10-25 19:10:47 -0300

    Revert "teste"
    This reverts commit 764fa7e87e8d2942608256a1e66cee6236c78c5f.

--- src/views/CadastroDeOrientadores.vue
+++ src/views/CadastroDeOrientadores.vue
@@ -146,7 +146,9 @@ async function handleSubmit(event) {
           </div>
           <button type="submit" class="btn-custom">Criar Conta</button>
         </form>
-        
+        <p class="text-center mt-3">
+          Já tem uma conta? <router-link to="login">Faça login.</router-link>
+        </p>
       </div>
     </div>
     <div v-else>
