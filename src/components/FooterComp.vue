e5c70a830b6820006c825af5f48c2675d9559538
Maria Helena Baldan Lemes <mariahelena1119@gmail.com>
2024-11-22 09:39:46 -0300

    mudado o tamanho da logo do if no footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -17,9 +17,9 @@
             <img src="../assets/images/suap_logo.png" style="width: 50px;">
           </a>
         </li>
-        <li class="ms-3 mt-1">
+        <li class="mt-1">
           <a class="p-2" href="https://spo.ifsp.edu.br/menu/191-estagios-documentos">
-            <img src="../assets/images/if_logo_2.png" style="width: 30px;">
+            <img src="../assets/images/if_logo_2.png" style="width: 23px;">
           </a>
         </li>
       </ul>
@@ -33,26 +33,3 @@ footer {
     width: 100%;
 }
 </style>
-
-<!-- <template>
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-1">
-        <div class="col-md-4 d-flex align-items-center">
-            <router-link to="home"><img src="../assets/images/FRAN_logo.png" width="40px" style="border-radius: 50%; margin-left: 15px;"></router-link>
-        </div>
-        <div>
-            <span class="mb-3">© 2024 FRAN Solutions, Inc. Todos os direitos reservados.</span>
-        </div>
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="https://suap.ifsp.edu.br/accounts/login/?next=/"><img src="../assets/images/suap_logo.png" style="width: 60px; margin-right: 10px; margin-top: 6px;"></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="https://spo.ifsp.edu.br/menu/191-estagios-documentos"><img src="../assets/images/if_logo_2.png" style="width: 30px; margin-right: 20px; margin-top: 8px;"></a></li>
-        </ul>
-    </footer>
-</template>
-
-<style scoped>
-footer {
-    background-color: #01400B;
-    color: white;
-    width: 100%;
-}
-</style> -->
