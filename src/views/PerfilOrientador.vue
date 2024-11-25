438f9cfe255a5c41a489767e2cc3e3130bd508eb
Igor Aguiar <102562449+AguiarIgor@users.noreply.github.com>
2024-11-24 21:17:38 -0300

    finalizando o formulário de atualização das informações do orientador no PerfilOrientador

--- src/views/PerfilOrientador.vue
+++ src/views/PerfilOrientador.vue
@@ -16,15 +16,31 @@ import user6 from '@/assets/images/user6.png'
 import user7 from '@/assets/images/user7.png'
 import user8 from '@/assets/images/user8.png'
 import user9 from '@/assets/images/user9.png'
+import { BForm, BFormGroup, BFormInput, BFormInvalidFeedback } from 'bootstrap-vue-3'
 
 axios.defaults.withCredentials = true
 
 const orientador = ref(null)
+const showModal = ref(false)
 const editando = ref(false)
 const imagemAleatoria = ref('')
+const imagens = [user1, user2, user3, user4, user5, user6, user7, user8, user9]
+
+const senha = ref('')
+const confirmarSenha = ref('')
+const senhaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 const errorMessage = ref('')
 
-const imagens = [user1, user2, user3, user4, user5, user6, user7, user8, user9]
+const touched = ref({
+  senha: false,
+  confirmarSenha: false,
+})
+
+const errors = ref({
+  senha: false,
+  confirmarSenha: false,
+})
+
 
 // Função para selecionar uma imagem aleatória
 const escolherImagemAleatoria = () => {
@@ -74,7 +90,65 @@ const editarInput = event => {
     const input = botaoClicado.parentElement.previousElementSibling
     input.disabled = !input.disabled
   }
+
+  if(botaoClicado.innerText === 'Editar') {
+    botaoClicado.innerText = 'Salvar';
+  } else {
+    botaoClicado.innerText = 'Editar';
+  }
 }
+
+function showingModal() {
+  if(showModal.value){
+    errors.value.senha = false;
+    errors.value.confirmarSenha = false;
+    touched.value.senha = false;
+    touched.value.confirmarSenha = false;
+    errorMessage.value = ''
+  }
+  showModal.value = !showModal.value;
+}
+
+function handleBlur(field) {
+  touched.value[field] = true;
+  if (field === "senha") {
+    errors.value.senha = !senhaRegex.test(senha.value);
+  } else if (field === "confirmarSenha") {
+    errors.value.confirmarSenha = senha.value !== confirmarSenha.value;
+  }
+}
+
+function validateFormPassword() {
+  errors.value = {
+    senha: !senhaRegex.test(senha.value),
+    confirmarSenha: senha.value !== confirmarSenha.value,
+  };
+  console.log(errors.value)
+  return Object.values(errors.value).some(error => error);
+}
+
+async function handleSubmitPassword() {
+  if(!senha.value || !confirmarSenha.value) return errorMessage.value = 'Preencha Todos os Campos!';
+  else if(validateFormPassword()) return;
+
+  try {
+    const orientador = {
+      password: senha.value,
+    }
+    const response = await axios.post(
+      'http://localhost:8082/FRAN/orientadores/', // Ta faltando colocar a URL correta
+      orientador
+    )
+
+    console.log(response)
+    showingModal();
+
+  } catch (error) {
+    errorMessage.value = error.response?.data || error.message
+    console.log('Erro ao atualizar a senha: ' + (error.response?.data || error.message))
+  }
+}
+
 </script>
 
 <template>
@@ -89,9 +163,9 @@ const editarInput = event => {
             :src="imagemAleatoria"
             alt="Imagem do orientador"
             width="20%"
-            class="mb-3 mt-4"
+            class="my-5"
           />
-          <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+
           <!-- Inputs -->
           <div>
             <!-- Nome -->
@@ -159,7 +233,7 @@ const editarInput = event => {
             <!-- Senha -->
             <div
               v-if="orientador"
-              class="row g-3 align-items-center justify-content-between mb-3"
+              class="row g-3 align-items-center justify-content-between mb-5"
             >
               <div class="col-auto">
                 <label for="inputSenha" class="col-form-label"> Senha: </label>
@@ -172,7 +246,7 @@ const editarInput = event => {
                   id="inputSenha"
                   disabled
                 />
-                <BotaoComp titulo="Editar" tamanho="p" />
+                <BotaoComp @click="showingModal" titulo="Editar" tamanho="p" />
               </div>
             </div>
           </div>
@@ -190,6 +264,35 @@ const editarInput = event => {
       </div>
     </main>
     <FooterComp />
+
+    <!-- Modal de Senha -->
+    <div v-if="showModal" v-show="showModal" class="modal-overlay" @click.self="showingModal">
+      <div class="form-container">
+        <BForm @submit.prevent="handleSubmitPassword">
+          <BFormGroup label="Senha:">
+            <BFormInput
+              v-model="senha"
+              type="password"
+              :state="touched.senha ? !errors.senha : null"
+              @blur="handleBlur('senha')"
+            />
+            <BFormInvalidFeedback class="text-center" v-if="errors.senha">Senha deve ter pelo menos 8 caracteres, incluindo letras e números.</BFormInvalidFeedback>
+          </BFormGroup>
+          <BFormGroup label="Confirmar Senha:">
+            <BFormInput
+              v-model="confirmarSenha"
+              type="password"
+              :state="touched.confirmarSenha ? !errors.confirmarSenha : null"
+              @blur="handleBlur('confirmarSenha')"
+            />
+            <BFormInvalidFeedback class="text-center" v-if="errors.confirmarSenha">As senhas não coincidem.</BFormInvalidFeedback>
+          </BFormGroup>
+          <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+          <BotaoComp titulo="Salvar Senha" type="submit" />
+        </BForm>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -198,68 +301,54 @@ input {
   width: 300px;
 }
 
+.form-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 350px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+/* Remove o fundo azul nos campos */
+.form-control {
+  background-color: transparent !important;
+  border-color: #ced4da; /* Cor padrão para borda */
+  transition: border-color 0.2s, box-shadow 0.2s;
+}
+
+/* Verde para válido */
+.form-control.is-valid {
+  background-color: transparent !important;
+  border-color: #28a745 !important;
+  box-shadow: 0 0 0 0.2rem rgba(40, 167, 69, 0.25);
+}
+
+/* Vermelho para inválido */
+.form-control.is-invalid {
+  background-color: transparent !important;
+  border-color: #dc3545 !important;
+  box-shadow: 0 0 0 0.2rem rgba(220, 53, 69, 0.25);
+}
+
 .error-message {
   color: red;
   text-align: center;
   margin-top: 10px;
 }
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
 </style>
-
-<!-- <script setup>
-import { ref, onMounted } from 'vue';
-import HeaderLogado from '@/components/HeaderLogado.vue'
-import FooterComp from '@/components/FooterComp.vue'
-import FileNavComp from '@/components/FileNavComp.vue'
-import BotaoComp from '@/components/BotaoComp.vue';
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-import '@/assets/css/global.css'
-
-const orientador = ref(null);
-onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:8082/FRAN/orientadores/me');
-    orientador.value = response.data;
-  } catch (error) {
-    console.error("Erro ao buscar orientador:", error);
-  }
-});
-
-</script>
-
-<template>
-  <div>
-    <HeaderLogado />
-    <main class="conteudo mt-0">
-      <FileNavComp />
-      <div
-        class="container text-center"
-      >
-        <div class="row">
-          <div
-            class="col d-flex flex-column align-items-center"
-          >
-            <img
-              src="@/assets/images/user.png"
-              alt=""
-              width="20%"
-              class="mt-2"
-            />
-            <h2 class="mb-5">{{ orientador ? orientador.nome : "Carregando..." }}</h2>
-            <h4>{{ orientador ? orientador.prontuario : "Carregando..." }}</h4>
-            <h4>{{ orientador ? orientador.email : "Carregando..." }}</h4>
-
-
-            <h4 class="mb-5">{{ orientador ? orientador.telefone : "Carregando..." }}</h4>
-
-            <BotaoComp titulo="Editar dados cadastrados" tamanho="g"/>
-          </div>
-        </div>
-      </div>
-    </main>
-    <FooterComp />
-  </div>
-</template>
-
-<style scoped></style>
- -->
